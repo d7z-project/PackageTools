e55f97628639464aaa38c57d205c848e3435308e
Dragon <ExplodingFKL@gmail.com>
2023-01-04 17:29:53 +0800

    feat: 打包配置

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use boa_engine::Context;
+
 fn main() {
-    println!("Hello, world!");
+    let mut context = Context::default();
+    println!("{:?}", context.eval(r#"
+    function hello(){
+    console.log("hello world");
+    return 1;
+    }
+    "#).unwrap());
+    println!("{:?}", context.eval("hello()").unwrap());
 }
